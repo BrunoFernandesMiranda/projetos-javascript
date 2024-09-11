c581cea1bd44db49ae0e3d87263d483e3655fb85
BrunoFernandesMiranda <155913874+BrunoFernandesMiranda@users.noreply.github.com>
2024-09-11 13:37:33 -0300

    calculadora

--- pequena calcuradora/js.js
+++ pequena calcuradora/js.js
@@ -1,12 +1,23 @@
-
-
-function ad(num1, num2) {
-
-    const num1 = parseFloat(document.getElementById('num1').value);
-    const num2 = parseFloat(document.getElementById('num2').value);
-    
-    let soma = num1 + num2;
-
-    document.getElementById('resultado').innerText(soma);
-    return false
+function insert(num) {
+    let numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + num;
 };
+
+function clean() {
+    document.getElementById('resultado').innerHTML = ""
+
+};
+
+function back(){
+    let resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1)
+};
+
+function calcular() {
+    let resultado = document.getElementById('resultado').innerHTML;
+
+    if(resultado){
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+
+}
